e3d6d5d4bfe20a7954ff7025c986f7e7cd99c385
Antonio Duma <ec2-user@ip-172-16-12-156.eu-west-1.compute.internal>
2019-08-27 15:53:44 +0000

    Added mleap serialization

--- 02_data_exploration_and_feature_eng/my_etl.py
+++ 02_data_exploration_and_feature_eng/my_etl.py
@@ -1,15 +1,31 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import time
 import sys
-from awsglue.transforms import *
+import os
+import shutil
+import csv
+import boto3
+import pyspark
+import zipfile
+import tarfile
+
+from time import gmtime, strftime
 from awsglue.utils import getResolvedOptions
+
+import mleap.pyspark
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.sql.types import StructField, StructType, StringType, DoubleType, FloatType
+from pyspark.ml.feature import StringIndexer, VectorIndexer, OneHotEncoder, VectorAssembler
+from pyspark.sql.functions import *
+from mleap.pyspark.spark_support import SimpleSparkSerializer
+
+from awsglue.transforms import *
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.ml.feature import StringIndexer
-from pyspark.ml.feature import OneHotEncoder
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.feature import Imputer
-from pyspark.ml import Pipeline
-from pyspark.sql.types import FloatType
 
 def csv_line(data):
     r = ','.join(str(d) for d in data[1])
@@ -18,22 +34,19 @@ def csv_line(data):
 glueContext = GlueContext(SparkContext.getOrCreate())
 spark = glueContext.spark_session
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'bucket'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_BUCKET'])
+
+# This is needed to save RDDs which is the only way to write nested Dataframes into CSV format
+spark.sparkContext._jsc.hadoopConfiguration().set("mapred.output.committer.class",
+                                                  "org.apache.hadoop.mapred.FileOutputCommitter")
+
 
 windturbine_rawdata = glueContext.create_dynamic_frame.from_catalog(
     database="endtoendml-db", table_name="data")
 
 df = windturbine_rawdata.toDF()
 df = df.na.replace('', "HAWT", subset=["turbine_type"])
-
-df = df.withColumn("wind_speed", df["wind_speed"].cast(FloatType()))
-df = df.withColumn("RPM_blade", df["RPM_blade"].cast(FloatType()))
-df = df.withColumn("col4float", df["col4float"].cast(FloatType()))
-df = df.withColumn("oil_level", df["oil_level"].cast(FloatType()))
-df = df.withColumn("temperature", df["temperature"].cast(FloatType()))
-df = df.withColumn("humidity", df["humidity"].cast(FloatType()))
-df = df.withColumn("vibrations_frequency", df["vibrations_frequency"].cast(FloatType()))
-df = df.withColumn("pressure", df["pressure"].cast(FloatType()))
+df = df.na.fill(37.0, subset=["col4float"])
 
 col0_indexer = StringIndexer(inputCol="turbine_id", outputCol="indexed_turbine_id")
 col1_indexer = StringIndexer(inputCol="turbine_type", outputCol="indexed_turbine_type")
@@ -44,35 +57,48 @@ turbine_id_encoder = OneHotEncoder(inputCol="indexed_turbine_id", outputCol="tur
 turbine_type_encoder = OneHotEncoder(inputCol="indexed_turbine_type", outputCol="turb_type").setDropLast(False)
 wind_direction_encoder = OneHotEncoder(inputCol="indexed_wind_direction", outputCol="wind_dir").setDropLast(False)
 
-col4_imputer = Imputer(inputCols=["col4float"], outputCols=["oil_temp"], strategy="median")
+#col4_imputer = Imputer(inputCols=["col4float"], outputCols=["oil_temp"], strategy="median")
 
-pipeline = Pipeline(stages=[col0_indexer, col1_indexer, col10_indexer, col11_indexer, turbine_id_encoder, turbine_type_encoder, wind_direction_encoder, col4_imputer])
+assembler = VectorAssembler(inputCols=['turb_id', 'turb_type', 'wind_speed', 'rpm_blade', 'col4float', 'oil_level','temperature', 
+              'humidity', 'vibrations_frequency', 'pressure', 'wind_dir'], outputCol="features")
+
+pipeline = Pipeline(stages=[col0_indexer, col1_indexer, col10_indexer, col11_indexer, turbine_id_encoder, turbine_type_encoder, wind_direction_encoder, assembler])
 
 model = pipeline.fit(df)
 df = model.transform(df)
 
-df = df.withColumnRenamed("indexed_breakdown", "break_down")
-
-assembler = VectorAssembler(inputCols=['turb_id', 'turb_type', 'wind_speed', 'RPM_blade', 'oil_temp', 'oil_level','temperature', 
-              'humidity', 'vibrations_frequency', 'pressure', 'wind_dir'], outputCol="features")
-
-df = assembler.transform(df)
-
-df = df.select('break_down', 'features')
-
-# This is needed to save RDDs which is the only way to write nested Dataframes into CSV format
-spark.sparkContext._jsc.hadoopConfiguration().set("mapred.output.committer.class",
-                                                  "org.apache.hadoop.mapred.FileOutputCommitter")
 
 # Split the overall dataset into 80-20 training and validation
 (train_df, validation_df) = df.randomSplit([0.8, 0.2])
     
 # Convert the train dataframe to RDD to save in CSV format and upload to S3
-train_rdd = train_df.rdd.map(lambda x: (x.break_down, x.features))
+train_rdd = train_df.rdd.map(lambda x: (x.indexed_breakdown, x.features))
 train_lines = train_rdd.map(csv_line)
-train_lines.saveAsTextFile('s3://{0}/data/preprocessed/train'.format(args['bucket']))
+train_lines.saveAsTextFile('s3://{0}/data/preprocessed/train'.format(args['S3_BUCKET']))
 
 # Convert the validation dataframe to RDD to save in CSV format and upload to S3
-validation_rdd = validation_df.rdd.map(lambda x: (x.break_down, x.features))
+validation_rdd = validation_df.rdd.map(lambda x: (x.indexed_breakdown, x.features))
 validation_lines = validation_rdd.map(csv_line)
-validation_lines.saveAsTextFile('s3://{0}/data/preprocessed/val'.format(args['bucket']))
+validation_lines.saveAsTextFile('s3://{0}/data/preprocessed/val'.format(args['S3_BUCKET']))
+
+# Serialize and store the model via MLeap
+timestamp = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+model_filename = '/tmp/model-' + timestamp + '.zip'
+SimpleSparkSerializer().serializeToBundle(model, 'jar:file:' + model_filename, validation_df)
+
+# Unzip the model as SageMaker expects a .tar.gz file but MLeap produces a .zip file
+    
+with zipfile.ZipFile(model_filename) as zf:
+    zf.extractall("/tmp/model-" + timestamp)
+
+# Write back the content as a .tar.gz file
+    
+with tarfile.open("/tmp/model-" + timestamp + ".tar.gz", "w:gz") as tar:
+    tar.add("/tmp/model-" + timestamp + "/bundle.json", arcname='bundle.json')
+    tar.add("/tmp/model-" + timestamp + "/root", arcname='root')
+    
+# Upload the model in tar.gz format to S3 so that it can be used with SageMaker for inference later
+s3 = boto3.resource('s3') 
+#file_name = os.path.join(args['bucket'], '/model/', 'model.tar.gz')
+s3.Bucket(args['S3_BUCKET']).upload_file('/tmp/model-' + timestamp + '.tar.gz', 'model/model.tar.gz')
+
